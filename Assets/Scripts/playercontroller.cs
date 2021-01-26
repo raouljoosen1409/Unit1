1dedd3abf87d03bb62fe8d962cf3e8d74ecf60d9
Itsrazzx <330963@student.mboutrecht.nl>
2021-01-26 09:02:12 +0100

    Unit 1

---
+++ Assets/Scripts/playercontroller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    public float speed = 40f;
+
+    public float turnspeed = 180f;
+
+    float horizontalInput;
+    float verticalInput; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward *Time.deltaTime * speed);
+        transform.Rotate(Vector3.up * Time.deltaTime * horizontalInput * turnspeed);
+    }
+}
